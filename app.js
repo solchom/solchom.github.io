fb2dfa32a1121ddee85e61e63255382f47760dcc
solchom <121071143+solchom@users.noreply.github.com>
2022-12-27 13:59:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-'use strict';
-
-const switcher = document.querySelector('.btn');
-
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
-
-    console.log('current class name: ' + className);
-});
+function newPage() {
+    window.location.href =
+'https://www.naver.com'
+}
